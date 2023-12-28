77e4df04d3fdc5ff95c558742759678f62ee4db6
ダニー <98234578+mrdannyarmstrong@users.noreply.github.com>
2023-12-27 19:20:48 -0500

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,13 @@
+﻿function clockwork() {
+    time = document.getElementById("time");
+    t = new Date();
+    min = '';
+    if (t.getMinutes() < 10) {
+       min = '0' + t.getMinutes()
+    }
+    else {
+        min = t.getMinutes()
+    }
+    fulltime = t.getHours() + ':' + min;
+    clock.innerText = fulltime
+}
